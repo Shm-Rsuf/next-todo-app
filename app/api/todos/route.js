b615f186c74ca6d67c1e20d05c1b8d0af283edf3
shm_usuf <shmusuf@gmail.com>
2023-08-01 10:59:16 +0600

    get request added

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -2,9 +2,34 @@ import { NextResponse } from "next/server";
 import connectMongoDb from "../../../libs/mongoDb";
 import Todo from "../../../models/todo.model";
 
+/* POST A TODO */
 export async function POST(req) {
-  const { text, status } = await req.json();
-  await connectMongoDb();
-  await Todo.create({ text, status });
-  return NextResponse.json({ message: "Todo is created" }, { status: 200 });
+  try {
+    const { text, status } = await req.json();
+    await connectMongoDb();
+    await Todo.create({ text, status });
+    return NextResponse.json({ message: "Todo is created" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+/* GET ALL TODOS */
+export async function GET() {
+  try {
+    await connectMongoDb();
+    const todos = await Todo.find();
+    if (!todos) {
+      return NextResponse.json({ message: "Todos not found" }, { status: 404 });
+    }
+    return NextResponse.json({ todos }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
