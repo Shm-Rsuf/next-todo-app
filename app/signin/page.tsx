af1d930f1054b01ae4f043e77e41706cf8681f86
shm_usuf <shmusuf@gmail.com>
2023-08-03 21:54:05 +0600

    all are done

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,11 +2,37 @@
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import style from "../sass/login.module.scss";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const SignInPage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const signInWithGoogle = async () => {
+    try {
+      await signIn("google");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [router, session]);
+
+  if (session) {
+    return null;
+  }
+
   return (
     <div className={style.login_container}>
       <div>
-        <button onClick={() => signIn("google")} className={style.loginBtn}>
+        <button onClick={signInWithGoogle} className={style.loginBtn}>
           <FcGoogle />
           Sign in with google
         </button>
